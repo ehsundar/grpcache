6b27a65a2bead33a9098181b154ab968aa235c7a
Amir Mohamad Ehsandar <amir.ehsandar@divar.ir>
2020-07-31 19:04:13 +0430

    Create redis cache naive interceptor

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/a3m7e7/grpcache
+
+go 1.14
+
+require (
+	github.com/gomodule/redigo v1.8.2
+	google.golang.org/grpc v1.31.0
+	google.golang.org/protobuf v1.25.0
+)
---
+++ interceptors.go
@@ -0,0 +1,59 @@
+package grpcache
+
+import (
+	"context"
+	"github.com/gomodule/redigo/redis"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+	"time"
+)
+
+type RedisCacheConfig struct {
+	Host               string
+	RequestUniqueIdExt func(req interface{}) (string, error)
+	TTL                time.Duration
+}
+
+func WithRedisCacheInterceptor(config RedisCacheConfig) grpc.DialOption {
+	rdb, err := redis.DialURL(config.Host)
+	if err != nil {
+		panic(err)
+	}
+
+	return grpc.WithUnaryInterceptor(
+		func(
+			ctx context.Context,
+			method string, req, reply interface{},
+			cc *grpc.ClientConn,
+			invoker grpc.UnaryInvoker,
+			opts ...grpc.CallOption,
+		) error {
+			key, err := config.RequestUniqueIdExt(req)
+			if err != nil {
+				return err
+			}
+
+			cachedReply, err := rdb.Do("GET", key)
+			if err != nil {
+				if err == redis.ErrNil {
+					err := invoker(ctx, method, req, reply, cc, opts...)
+
+					serializedReply, err := proto.Marshal(reply.(proto.Message))
+					if err != nil {
+						return err
+					}
+
+					ttl := int32(config.TTL / time.Second)
+					_, err = rdb.Do("SETEX", key, ttl, serializedReply)
+					return err
+				}
+				return err
+			}
+
+			err = proto.Unmarshal(cachedReply.([]byte), reply.(proto.Message))
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+}
